013df32e9c9c59f2eeb30aacece86a46d93b714a
Lawrence Chik <lchik001@fiu.edu>
2022-02-28 18:12:13 -0500

    Added functionality for wrong and correct letters

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header'
 import Figure from './components/Figure'
 import WrongLetters from './components/WrongLetters'
@@ -5,14 +6,48 @@ import Word from './components/Word'
 
 import './App.css';
 
+const words = ['application', 'programming', 'interface', 'wizard'];
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
 function App() {
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+
+  useEffect(() => {
+    const handleKeydown = event => {
+      const { key, keyCode } = event;
+        if (playable && keyCode >= 65 && keyCode <= 90) {
+          const letter = key.toLowerCase();
+    
+          if (selectedWord.includes(letter)) {
+            if (!correctLetters.includes(letter)) {
+              setCorrectLetters(currentLetters => [...currentLetters, letter]);
+            } else {
+              //showNotification();
+            }
+          } else {
+            if (!wrongLetters.includes(letter)) {
+              setWrongLetters(wrongLetters => [...wrongLetters, letter]);
+            } else {
+              //showNotification();
+            }
+        }
+      }
+    }
+    window.addEventListener('keydown', handleKeydown);
+
+    return () => window.removeEventListener('keydown', handleKeydown);
+
+  }, [correctLetters, wrongLetters, playable]);
+
   return (
     <>
       <Header />
       <div className="game-container">
         <Figure />
-        <WrongLetters />
-        <Word />
+        <WrongLetters wrongLetters={wrongLetters}/>
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
       </div>
     </>
   );
